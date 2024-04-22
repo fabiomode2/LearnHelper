d3f0f7c71eb28e44c4a6b74f49f6a8b4a4435a7b
fabiomode2 <fabiomancebotrillo35@gmail.com>
2024-04-22 20:09:37 +0200

    a tomar polo cu

--- src/components/AskText.tsx
+++ src/components/AskText.tsx
@@ -1,11 +1,5 @@
 import { useState } from "react";
-// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { seleccionarFrases } from "./utils";
-
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-
-import { Container, Item } from "./DropableContainer";
 
 interface Props {
   text: string;
@@ -16,31 +10,38 @@ const AskText = ({ text }: Props) => {
 
   let frases_seleccionadas: string[] = [];
 
-  const [cardItems, changeCardItems] = useState<Item[]>([]);
-
+  const [fraseCorrecta, changeFraseCorrecta] = useState("");
   const [clueSentences, changeClueSentences] = useState<String[]>([
     "Primera frase",
     "Ultima frase",
   ]);
-  const [sentecesToOrder, ChangeSentecesToOrder] = useState<String[]>([]);
   const [alreadyAsked, changeAlreadyAsked] = useState(true);
+  const [frasesASeleccionar, changeFrasesASeleccionar] = useState<String[]>([]);
+  const [fraseFinal, ChangeFraseFinal] = useState("");
 
-  const verificarRespuestas = () => {};
+  const verificarRespuestas = (frase: String) => {
+    changeAlreadyAsked(true);
+
+    console.log(frase);
+    console.log(fraseCorrecta);
+
+    if (frase == fraseCorrecta) {
+      ChangeFraseFinal("✅Correct!✅");
+    } else {
+      ChangeFraseFinal("❌Incorrect❌");
+    }
+  };
 
   const generarPregunta = () => {
+    changeAlreadyAsked(false);
+    ChangeFraseFinal("");
+
     const nuevasFrases = seleccionarFrases(text_dividido);
-    changeClueSentences([nuevasFrases[0], nuevasFrases[-1]]);
-    frases_seleccionadas = nuevasFrases.slice(1, 4);
 
-    console.log(frases_seleccionadas);
-
-    let lista: Item[] = [];
-    sentecesToOrder.map((item, index) => {
-      let a: Item = { id: index, text: item.toString() };
-      lista.push(a);
-    });
-
-    changeCardItems(lista);
+    changeClueSentences([nuevasFrases[1], nuevasFrases[3]]);
+    frases_seleccionadas = [nuevasFrases[0], nuevasFrases[2], nuevasFrases[4]];
+    changeFraseCorrecta(nuevasFrases[2]);
+    changeFrasesASeleccionar(frases_seleccionadas);
   };
 
   return (
@@ -51,9 +52,9 @@ const AskText = ({ text }: Props) => {
             <p className="raleway-f coolp">{clueSentences[0]}</p>
           </li>
 
-          <DndProvider backend={HTML5Backend}>
-            <Container cardss={cardItems} />
-          </DndProvider>
+          <li className="listitem">
+            <p className="raleway-f coolp">?</p>
+          </li>
 
           <li className="listitem">
             <p className="raleway-f coolp">{clueSentences[1]}</p>
@@ -61,7 +62,24 @@ const AskText = ({ text }: Props) => {
         </ol>
       </div>
 
-      {alreadyAsked ? (
+      <div className="flex-row">
+        {frasesASeleccionar.map((item, index) => (
+          <>
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={() => verificarRespuestas(item)}
+              key={index}
+            >
+              {item}
+            </button>
+          </>
+        ))}
+      </div>
+
+      <h3 className="mm-2">{fraseFinal}</h3>
+
+      {alreadyAsked && (
         <>
           <button
             type="button"
@@ -69,16 +87,6 @@ const AskText = ({ text }: Props) => {
             onClick={generarPregunta}
           >
             Next Question
-          </button>
-        </>
-      ) : (
-        <>
-          <button
-            type="button"
-            className="btn btn-info"
-            onClick={verificarRespuestas}
-          >
-            Check Answers
           </button>
         </>
       )}
--- src/components/utils.ts
+++ src/components/utils.ts
@@ -17,14 +17,21 @@ export const ShuffleList = ( lista : any[]) => {
 
 }
 
+function randInt(min : number, max : number) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 export function seleccionarFrases(frases: string[]): string[] {
   let frasesSeleccionadas: string[] = [];
   
   if (frases.length >= 5) {
-    const indiceAleatorio = Math.floor(Math.random() * (frases.length - 4)) + 1;
-    frasesSeleccionadas = frases.slice(indiceAleatorio, indiceAleatorio + 3);
-    frasesSeleccionadas.unshift(frases[indiceAleatorio - 1]); // Añade la frase anterior
-    frasesSeleccionadas.push(frases[indiceAleatorio + 3]); // Añade la frase siguiente
+    // const indiceAleatorio = Math.floor(Math.random() * (frases.length - 4)) + 1;
+    // frasesSeleccionadas = frases.slice(indiceAleatorio, indiceAleatorio + 3);
+    // frasesSeleccionadas.unshift(frases[indiceAleatorio - 1]); // Añade la frase anterior
+    // frasesSeleccionadas.push(frases[indiceAleatorio + 3]); // Añade la frase siguiente
+
+    let primerIndice = randInt(0, frases.length - 5)
+    frasesSeleccionadas= [frases[primerIndice], frases[primerIndice + 1], frases[primerIndice + 2], frases[primerIndice + 3], frases[primerIndice + 4]]
   } else if (frases.length > 0) {
     frasesSeleccionadas = [...frases]; // Devuelve todas las frases si hay menos de 5
   }
